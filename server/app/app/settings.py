126d94f136a1739d1c34a4a53f8989489e46585e
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-03-24 18:09:48 +0500

    fixed settings.py to allow hosts

--- server/app/app/settings.py
+++ server/app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0srcv_hyk)sm^nnz#g@sf8mkejky5$24%v!a(d*xjq)1x59)^w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
