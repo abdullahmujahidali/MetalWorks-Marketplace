2ccc1e79d9081009934d58a0985eaf63597ba12c
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-03-25 10:17:22 +0500

    configured postgresql and removing sqlite fingerprint from app

--- server/app/app/settings.py
+++ server/app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,9 +75,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": 5432,
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
+        "ATOMIC_REQUESTS": True,
+        "TIME_ZONE": "UTC",
+        "AUTOCOMMIT": True,
     }
 }
 
