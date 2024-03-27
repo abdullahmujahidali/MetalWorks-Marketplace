1a7ef66f61001aed2af47d34e569baaec06ab19d
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-03-27 16:16:18 +0500

    Fixing landing

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,10 +4,11 @@ export const Header = () => {
   return (
     <div className="flex  py-3 mx-3  items-center justify-between  border-black border-b">
       <div className="flex gap-4 w-full">
+        <h2 className="font-bold">Metal Works</h2>
         <a href="#">Home</a>
         <a href="#">About</a>
       </div>
-      <Button>Home</Button>
+      <Button>Login</Button>
     </div>
   );
 };
--- client/src/views/Landing/Landing.tsx
+++ client/src/views/Landing/Landing.tsx
@@ -6,14 +6,14 @@ export default function Landing() {
     <div className="flex flex-col h-screen ">
       <Header />
       <div className="flex flex-col items-center justify-center h-full gap-4  w-full ">
-        <h1 className=" text-5xl">Welcome to Metal Works.</h1>
+        <h1 className=" text-5xl">Welcome to Metal Works</h1>
         <p className="max-w-[450px] text-center">
           Metal works is an inventory management system, which keeps tracks of
           your metal, iron or steel products, you can add/remove users
         </p>
         <div className="gap-2 flex ">
           <Button> Sign Up</Button>
-          <Button color="red"> Login</Button>
+          <Button color="iris"> Login</Button>
         </div>
       </div>
     </div>
